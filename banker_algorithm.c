8ef3e2321abdc906664e7a5471aad766f0a27bba
calumsudo <66584928+calumsudo@users.noreply.github.com>
2022-05-13 00:06:31 -0400

    bug fixes

--- banker_algorithm.c
+++ banker_algorithm.c
@@ -9,57 +9,51 @@ int banker(struct Job *job, struct Request_devices *dev_req, struct Release_devi
 	//int need = job->memory_required;
 	int resources = job->devices_required;
 	//int max_memory = sys_status->memory_available;
-
-
 	printf("pcount =%d\n", p_count);
 	printf("process =%d\n", process);
 	printf("available resources =%d\n", available_resources);
 	printf("need =%d\n", job->memory_required);
 	printf("resources =%d\n", resources);
 
-	if(process != NULL && p_count == NULL){
+
+//There must be a better way to count how many processes there are (attention needed)
+	if(process != 0 && p_count == 0){
 		p_count++;
-		printf("test1\n");
+		//printf("test1\n");
 	}
-	else if(process != NULL && p_count == 0){
+	else if(process != 0 && p_count > 0){
 		p_count ++;
-		printf("test2\n");
-	}
-	else if(process != NULL && p_count > 0){
-		p_count ++;
-		printf("test3\n");
+		//printf("test2\n");
 	}
 	else{
-		printf("%d",p_count);
-		printf("test4\n");
+		printf("Fatal Error\n");
 	}
 
-	int alloc[p_count][resources];
+	int alloc[p_count][resources];//allocation matrix 
 
 
-printf("Allocation =%d\n", alloc);
+printf("Allocation =%d\n", alloc[p_count][resources]);
 	
-int max[process][resources];
-int available[available_resources];
-
-
+int max[process][resources]; //max resources matrix
+int available[available_resources]; //available resources
+int need[process][resources]; //resources needed
 
 
 	int k,j,i;
+  int y = 0;
     int f[process], ans[process], ind = 0;
-    for (k = 0; k < process; k++) {
+    for (k = 0; k < process; k++){
         f[k] = 0;
     }
-    int need[process][resources];
-    for (i = 0; i < process; i++) {
+    
+    for (i = 0; i < process; i++){
         for (j = 0; j < resources; j++)
             need[i][j] = max[i][j] - alloc[i][j];
     }
-    int y = 0;
-    for (k = 0; k < process; k++) {
-        for (i = 0; i < process; i++) {
+    
+    for (k = 0; k < process; k++){
+        for (i = 0; i < process; i++){
             if (f[i] == 0) {
- 
                 int flag = 0;
                 for (j = 0; j < resources; j++) {
                     if (need[i][j] > available[j]){
@@ -67,7 +61,6 @@ int available[available_resources];
                          break;
                     }
                 }
- 
                 if (flag == 0) {
                     ans[ind++] = i;
                     for (y = 0; y < resources; y++)
@@ -77,25 +70,20 @@ int available[available_resources];
             }
         }
     }
-   
       int flag = 1;
-       
-      for(int i=0;i<process;i++)
-    {
-      if(f[i]==0)
-      {
-        flag=0;
-         printf("The system is not safe\n");
-        break;
+      for(int i=0;i<process;i++){
+        if(f[i]==0){
+          flag=0;
+          printf("The system is not safe\n");
+          break;
+        }
       }
-    }
-     
-      if(flag==1)
-    {
-      printf("This sequence is safe\n");
-      for (i = 0; i < process - 1; i++)
-        printf(" P%d ->", ans[i]);
-      printf(" P%d\n", ans[process - 1]);
-    }
-return 0;
+      if(flag==1){
+        printf("This sequence is safe\n");
+        for (i = 0; i < process - 1; i++){
+          printf(" P%d ->", ans[i]);
+          printf(" P%d\n", ans[process - 1]);
+        }
+      }
+return (0);
 }
