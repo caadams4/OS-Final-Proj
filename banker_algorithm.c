e6a68031f0304c2850b8bea0a094aa15168f2036
calumsudo <66584928+calumsudo@users.noreply.github.com>
2022-05-16 22:28:28 -0400

    Add files via upload

--- banker_algorithm.c
+++ banker_algorithm.c
@@ -1,56 +1,25 @@
 // Banker's Algorithm
 #include "prototypes.h"
-int p_count;
 
 int banker(struct Job *job, struct Request_devices *dev_req, struct Release_devices *dev_rel, struct System_status *sys_status)
 {	
 	int process = job->job_number;
 	int available_resources = sys_status->memory_available;
-	//int need = job->memory_required;
-	int resources = job->devices_required;
-	//int max_memory = sys_status->memory_available;
-	printf("pcount =%d\n", p_count);
-	printf("process =%d\n", process);
-	printf("available resources =%d\n", available_resources);
-	printf("need =%d\n", job->memory_required);
-	printf("resources =%d\n", resources);
-
-
-//There must be a better way to count how many processes there are (attention needed)
-	if(process != 0 && p_count == 0){
-		p_count++;
-		//printf("test1\n");
-	}
-	else if(process != 0 && p_count > 0){
-		p_count ++;
-		//printf("test2\n");
-	}
-	else{
-		printf("Fatal Error\n");
-	}
-
-	int alloc[p_count][resources];//allocation matrix 
-
-
-printf("Allocation =%d\n", alloc[p_count][resources]);
-	
-int max[process][resources]; //max resources matrix
-int available[available_resources]; //available resources
-int need[process][resources]; //resources needed
-
-
+  int resources = job->devices_required;
+	int alloc[process][resources];//allocation matrix 	
+  int max[process][resources]; //max resources matrix
+  int available[available_resources]; //available resources
+  int need[process][resources]; //resources needed
 	int k,j,i;
   int y = 0;
     int f[process], ans[process], ind = 0;
     for (k = 0; k < process; k++){
         f[k] = 0;
     }
-    
     for (i = 0; i < process; i++){
         for (j = 0; j < resources; j++)
             need[i][j] = max[i][j] - alloc[i][j];
     }
-    
     for (k = 0; k < process; k++){
         for (i = 0; i < process; i++){
             if (f[i] == 0) {
